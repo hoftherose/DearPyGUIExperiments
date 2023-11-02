e92fd3894ae6d3c4e97654ad9887297e0c039ebd
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-11-02 10:30:34 -0600

    Add attribute types

--- src/interfaces/nodes.py
+++ src/interfaces/nodes.py
@@ -1,10 +1,14 @@
 class Attributes:
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, attribute_type=None, **kwargs):
         self.name = name
+        self.attribute_type = attribute_type
         self.params = kwargs
 
     def GetName(self) -> str:
         return self.name
+
+    def GetAttributeType(self) -> str:
+        return self.attribute_type
 
     def GetParams(self) -> dict[str, any]:
         return self.params
